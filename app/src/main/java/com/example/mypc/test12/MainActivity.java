82ce9124f3e5a0d758148b7d7d611cc7af90807d
dansigit <e.dansi.111@gmail.com>
2019-08-06 12:28:05 +0430

    commit

--- app/src/main/java/com/example/mypc/test12/MainActivity.java
+++ app/src/main/java/com/example/mypc/test12/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         int i=0;
         Toast.makeText(getApplicationContext(),"i="+i,Toast.LENGTH_LONG).show();
+        int j=0;
+        float h=0;
 
     }
 }
